a8ef9cc9c1db3795418794430b07a35cc11b82b5
AlexNimliienko <reconike@gmail.com>
2025-02-19 23:49:49 +0100

    get_weather created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import config
 import telebot
+import GetWeather
 
 bot = telebot.TeleBot(config.API_TOKEN)
 
@@ -9,7 +10,13 @@ bot = telebot.TeleBot(config.API_TOKEN)
 
 @bot.message_handler(commands=["start"])
 def answer_to_start(message):
-    bot.send_message(message.chat.id, 'You writed START')
+    bot.send_message(message.chat.id, 'Hello, Send me Your city name, and I`ll answer with information :)')
+
+@bot.message_handler(content_types=["text"])
+def get_weather(message):
+    weather = GetWeather.getWeather(message.text.lower())
+    bot.send_message(message.chat.id, weather)
+    print(type(message))
 
 if __name__ == '__main__':
      bot.infinity_polling()
